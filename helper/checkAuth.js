ecdf20ec5a00d5be341c15c5c4e6c4d519439046
trevinkur <105697744+trevinkur@users.noreply.github.com>
2022-07-12 14:53:39 +0700

    Update checkAuth.js

--- helper/checkAuth.js
+++ helper/checkAuth.js
@@ -1,32 +1,34 @@
 const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 function checkAuthAdmin(req,res, next) {
-    jwt.verify(req.headers.authorization, "mokeyJaya123", (err, decoded) => {
+    jwt.verify(req.headers.authorization, process.env.PRIVATE_KEY, (err, decoded) => {
         if(err) {
-            res.send({
+            res.status(401).send({
                 message: "unauthorized"
             })
         } else {
             if(decoded.role === "admin") {
                 next()
             } else {
-                res.send({message: "Forbidden"}) 
+                res.status(403).send({message: "Forbidden"}) 
             }
         }
     })
 }
 
 function checkAuthUser(req,res, next) {
-    jwt.verify(req.headers.authorization, "mokeyJaya123", (err, decoded) => {
+    jwt.verify(req.headers.authorization, process.env.PRIVATE_KEY, (err, decoded) => {
         if(err) {
-            res.send({
-                message: "unauthorized"
+            res.status(401).send({
+                message: "unauthorized",
+                status: 401
             })
         } else {
             if(decoded.role === "admin" || decoded.user_id == req.params.id) {
                 next()
             } else {
-                res.send({message: "Forbidden"}) 
+                res.status(403).send({message: "Forbidden"}) 
             }
         }
     })
